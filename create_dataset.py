972b1892fcbaae8ad7d1880a18ec1596773fedb0
mtthew-teng <196126034+mtthew-teng@users.noreply.github.com>
2025-02-14 21:22:35 -0800

    plot images to pyplot

---
+++ create_dataset.py
@@ -0,0 +1,24 @@
+import mediapipe as mp
+import cv2
+import os
+import matplotlib.pyplot as plt
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+DATA_DIR = './data'
+
+for dir_ in os.listdir(DATA_DIR):
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_))[:1]:
+        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        hands.process()
+
+        plt.figure()
+        plt.imshow(img_rgb)
+
+plt.show()
