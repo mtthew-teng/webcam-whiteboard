af906258175611b84f2d0c4abca4491cd17115c3
mtthew-teng <196126034+mtthew-teng@users.noreply.github.com>
2025-02-15 20:24:29 -0800

    save data as byte stream

--- create_dataset.py
+++ create_dataset.py
@@ -2,6 +2,7 @@ import mediapipe as mp
 import cv2
 import os
 import matplotlib.pyplot as plt
+import pickle
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -14,7 +15,7 @@ DATA_DIR = './data'
 data = []
 labels = []
 for dir_ in os.listdir(DATA_DIR):
-    for img_path in os.listdir(os.path.join(DATA_DIR, dir_))[:1]:
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
         data_aux = []
         img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -38,7 +39,6 @@ for dir_ in os.listdir(DATA_DIR):
             data.append(data_aux)
             labels.append(dir_)
 
-        plt.figure()
-        plt.imshow(img_rgb)
-
-plt.show()
+f = open('data.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f) 
+f.close()
