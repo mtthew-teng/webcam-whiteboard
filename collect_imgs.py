38ef8dc9794b89e8780d1940836d78b2f3f185db
mtthew-teng <196126034+mtthew-teng@users.noreply.github.com>
2025-02-19 18:01:28 -0800

    add third class for nongesture

--- collect_imgs.py
+++ collect_imgs.py
@@ -7,18 +7,17 @@ DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-num_classes = 2
-dataset_size = 100
+number_of_classes = 3
+dataset_size = 500
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-for i in range(num_classes):
-    if not os.path.exists(os.path.join(DATA_DIR, str(i))):
-        os.makedirs(os.path.join(DATA_DIR, str(i)))
+for j in range(number_of_classes):
+    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
+        os.makedirs(os.path.join(DATA_DIR, str(j)))
 
-    print('Collecting data for class {}'.format(i))
+    print('Collecting data for class {}'.format(j))
 
     done = False
-
     while True:
         ret, frame = cap.read()
         cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
@@ -27,13 +26,14 @@ for i in range(num_classes):
         if cv2.waitKey(25) == ord('q'):
             break
 
-        counter = 0
-        while counter < dataset_size:
-            ret, frame = cap.read()
-            cv2.imshow('frame', frame)
-            cv2.imwrite(os.path.join(DATA_DIR, str(i), '{}.jpg'.format(counter)), frame)
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
 
-            counter += 1
+        counter += 1
 
 cap.release()
 cv2.destroyAllWindows()
